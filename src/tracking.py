88ef6fa479fde876c6aac74946ceff1ceb9e30c7
Shawn5141 <s19911111@gmail.com>
2019-05-16 23:33:52 -0700

    tracking alg

--- src/tracking.py
+++ src/tracking.py
@@ -14,36 +14,59 @@ Tracking_suspect()
 Display()
 
 '''
+from dataType import humanData,itemData
+from matching import findClosestHuman
 
-'''
 def Scan_for_item_existing(humanDataset, itemDataset):
-	for all people in humanDataset:
-		if people.missing == false:
-			for item in people.itemList:
-				if item.missing == false: 
-					item.alarm_flag = true
-				else:
-					if item.alarm_flag == True:
-						Tracking_suspect(item)
-					else:
-						pop_item_from_dataset(item)
-			if people.itemList == []:
-				pop_people_from_dataset(people)
+    oclussion_check_dist=30   #not sure about this distance
+    for human in humanDataset.values():
+	if human.missing == True:  #True or false
+	    for item in human.itemList:
+		cloestHuman,dist=findCloestHuman(item,humanDataset) 
+		if itemDataset[item].missing == False: 
+		     itemDataset[item].alarm_flag = True
+                     if dist>oclussion_check_dist:
+                        cloestHuman.isSuspect=True
+                        Track_and_display(humanDataset) 
+		else:
+		    if itemDataset[item].alarm_flag == True:
+		        #cloestHuman,dist=findCloestHuman(item,humanDataset) 
+                        
+                        if cloestHuman.isSuspect==True:
+                            Track_and_display(humanDataset)   
+                        else:
+                            if dist>oclussion_chek_dist:
+                                cloestHuman.isSuspect=True
+                            else:
+                                cloestHuman.isSuspect=False    
+                            
+		    else:
+		        Pop_item_from_dataset(item)  #minor Case: disappear at same time
+	     if human.itemList == []:
+	         Pop_human_from_dataset(human)
+         else:
+             for item in human.itemList:
+                 if itemDataset[item].missing ==True:
+                     Pop_item_from_dataset(item)
                  else:
-                        for item in people.itemList:
-                                if item.missing ==true:
-                                        pop_item_from_dataset(item)    
+                     pass    
 
-										
-def Tracking_suspect(item):
-	for all people in humanDataset:
-		if people.position is close to item.position:
-			People.suspect_label = True
+										 
 
 
-def Display(people dataset):
-	for all people in dataset:
-		if people.suspect_label==true: bounded with red color
-             	else: bounded with black color
+def Track_and_Display(humanDataset):
+    human_disp_list=[]
+    for human in humandataset:
+	if human.isSuspect==true: 
+             #bounded with red color
+             human_disp_list.append([human.id,"red"])
+        else: 
+             #bounded with black color
+             human_disp_list.append([human.id,"black"])
+    print(human_disp_list)
 
-'''
+
+
+
+
+
